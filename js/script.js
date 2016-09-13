8882911be26918e7e78d6ce170a19750f1ac4dae
Karen Lee <kglee@ucsc.edu>
2016-09-12 17:27:35 -0700

    Update script.js
    Updated modal to view previous and next images using the respective buttons.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,80 @@
+// Main navigation
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='art.html'>Portfolio</a></li>" +
 "</ul>";
 
-function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    document.getElementById("img01").alt = element.alt;
-    document.getElementById("caption").innerHTML = element.alt;
-}
+// Modal slideshow
+$(document).ready(function ($) {
+    var i;
+    var count = $(".img-container").length - 1;
+
+    // Display modal on click
+    $(".img-container img").click(function () {
+        $(".modal").css("display", "block");
+        // Index starting at 0
+        i = $(".img-container img").index(this);
+        img_slideshow();
+    });
+
+    // Shows current slide
+    function img_slideshow() {
+        var curr = $(".img-container img:eq(" + i + ")");
+        $("#img01").attr("src", curr.attr("src"));
+        $("#img01").fadeOut(0);
+        $("#img01").fadeIn(300);
+        $("#caption").text(curr.attr("alt") + " [#" + (i+1) + " of "
+                            + (count+1) + "]");
+    };
+
+    $("#previous").click(function () {
+        previous_image();
+    });
+
+    $("#next").click(function () {
+        next_image();
+    });
+
+    $("#close").click(function() {
+        close_viewer();
+    });
+
+    function previous_image() {
+        if (i > 0) {
+            i = i - 1;
+        } else {
+            i = count;
+        };
+        img_slideshow();
+    };
+
+    function next_image() {
+        if (i < count) {
+            i = i + 1;
+        } else {
+            i = 0;
+        };
+        img_slideshow();
+    };
+
+    function close_viewer() {
+        $(".modal").css("display", "none");
+    };
+
+    // Left arrow (37), right arrow (39), escape (27) keys
+    $(document).keypress(function (e) {
+        if (e.keyCode == 37) {
+            previous_image();
+        }
+        if (e.keyCode == 39) {
+            next_image();
+        }
+        if (e.keyCode == 27) {
+            close_viewer();
+        }
+    });
+});
 
 var modal = document.getElementById("modal01");
 window.onclick = function(event) {
@@ -18,6 +83,7 @@ window.onclick = function(event) {
     }
 }
 
+// Portfolio filtering using MixItUp
 $(function () {
     var filterList = {
         init: function () {
@@ -27,7 +93,7 @@ $(function () {
                 filter: '.filter'
                 },
                 load: {
-                    filter: '.3D'
+                    filter: '.code'
                 }
             });
         }
