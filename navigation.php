878535e65660611ce9a40998f088bf9644ecbcf8
Karen Lee <kglee@ucsc.edu>
2016-09-13 17:17:23 -0700

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,10 @@
+<div id="nav01">
+    <ul id="menu">
+        <li>
+            <a <?php echo ($page == 'one') ? "class='active'" : ""; ?>
+              href="index.php">Home</a></li>
+        <li>
+            <a <?php echo ($page == 'two') ? "class='active'" : ""; ?>
+              href="art.php">Portfolio</a></li>
+    </ul>
+</div>
